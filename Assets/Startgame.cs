32d0e99a76fde28598509d63274880e616c97817
psyia3 <125268114+psyia3@users.noreply.github.com>
2023-02-16 20:25:09 +0000

    Update Startgame.cs
    Trying to push code for the start menu transition

--- Assets/Startgame.cs
+++ Assets/Startgame.cs
@@ -7,6 +7,8 @@ public class Startgame : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        SceneManager.LoadScene("SampleScene");
     }
+
+    
 }

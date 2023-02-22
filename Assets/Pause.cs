37a7622ba84a971faf37cbc433f6a78a70f4358e
Grubalub <69207566+Grubalub@users.noreply.github.com>
2023-02-22 17:17:27 +0000

    fix/Pause menu
    The game will now stop and start when 'p' is pressed

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -1,28 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class Pause : MonoBehaviour
 {
     [Header("Pause")]
     [SerializeField] private GameObject pauseScreen;
 
-    private void Update()
+    private void Awake()
+    {
+        pauseScreen.SetActive(false);
+    }
+
+      private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
             if(pauseScreen.activeInHierarchy)
-                PauseGame(false);
-            else 
+                 PauseGame(false);
+            else
                 PauseGame(true);
+                
         }
     }
 
 
-    private void PauseGame(bool status)
+    public void PauseGame(bool status)
     {
         pauseScreen.SetActive(status);
+
+        if (status)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
     }
 
     

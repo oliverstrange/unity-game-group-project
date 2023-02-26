4dfba4d70e52230551a7c2074932227ba5abe3c9
conrad8027 <125267636+conrad8027@users.noreply.github.com>
2023-02-26 19:14:57 +0000

    uploaded code for the timer
    A basic timer, still need to program the things that happen when the time is over (back to mainmenu? stop the game? losing screen?)

---
+++ Assets/timer.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    private float timeLeft = 30.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timerText.text = "Time left: " + Mathf.Round(timeLeft).ToString();
+
+        if (timeLeft < 0)
+        {
+            print("time's up")
+        }
+    }
+}

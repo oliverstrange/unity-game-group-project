338a7d44f66d90379e5fe3a4d77d55f35cdec996
conrad8027 <125267636+conrad8027@users.noreply.github.com>
2023-03-10 11:43:13 +0000

    updated timer script for winScreen

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class timer : MonoBehaviour
 {
     public Text timerText;
+    public Text usedTimeText;
     private float timeUsed = 0f;
 
     void Update()
     {
         timeUsed += Time.deltaTime;
         timerText.text = "Time Taken: " + Mathf.Round(timeUsed).ToString();
+        usedTimeText.text = "Time Taken: " + Mathf.Round(timeUsed).ToString()+"s";
     }
 }
